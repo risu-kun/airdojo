b0a65e2cd6eae9a97e4fac2b26159fc789d02f4c
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-20 19:22:56 +0000

    Require user authentication to access instructor view of course

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,5 +1,6 @@
 class Instructor::CoursesController < ApplicationController
 	before_action :authenticate_user!
+	before_action :require_authorized_for_current_course
 
 	def new
 		@course = Course.new
@@ -15,7 +16,6 @@ class Instructor::CoursesController < ApplicationController
 	end
 
 	def show
-		@course = Course.find(params[:id])
 	end
 
 	private
@@ -23,4 +23,14 @@ class Instructor::CoursesController < ApplicationController
 		params.require(:course).permit(:title, :description, :cost)
 	end
 
+	def require_authorized_for_current_course
+		if current_course.user != current_user
+			render text: "Unauthorized", status: :unauthorized
+		end
+	end
+
+	helper_method :current_course
+	def current_course
+		@current_course ||= Course.find(params[:id])
+	end
 end
