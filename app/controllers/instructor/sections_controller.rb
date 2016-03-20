fd07f47506324d7c1f7768644b14df099b8867b3
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-20 18:06:03 +0000

    Add controller and view for sections (form for instructors to create new sessions)

---
+++ app/controllers/instructor/sections_controller.rb
@@ -0,0 +1,20 @@
+class Instructor::SectionsController < ApplicationController
+	def new
+		@course = Course.find(params[:course_id])
+		@section = Section.new
+	end
+
+	def create
+		@course = Course.find(params[:course_id])
+		@section = @course.sections.create(section_params)
+
+		if @section.valid?
+			redirect_to instructor_course_path(@course)
+		end
+	end
+
+	private
+	def section_params
+		params.require(:section).permit(:title)
+	end
+end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,3 @@
 class Section < ActiveRecord::Base
-	belong_to :course
+	belongs_to :course
 end
