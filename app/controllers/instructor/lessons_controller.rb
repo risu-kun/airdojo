e9e7ed9fe698c727a889c63799c8ec390b96a309
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-20 18:52:39 +0000

    Hook up lesson creation form to lessons#create

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,5 +1,19 @@
 class Instructor::LessonsController < ApplicationController
 	def new
+		@section = Section.find(params[:section_id])
+		@lesson = Lesson.new
 	end
-	
+
+	def create
+		@section = Section.find(params[:section_id])
+		@lesson = @section.lessons.create(lesson_params)
+		if @lesson.valid?
+			redirect_to instructor_course_path(@section.course)
+		end
+	end
+
+	private
+	def lesson_params
+		params.require(:lesson).permit(:title, :subtitle)
+	end
 end
