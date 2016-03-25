d45af9743580c19219be5e39f982ee98253067a6
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-25 05:17:37 +0000

    Require users to be enrolled in a course to view the lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,24 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrolled_in_current_course
+
 	def show
 	end
 	
+	private
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
+	end
+
+	def current_course
+		@current_course = current_lesson.section.course
+	end
+
+	def require_enrolled_in_current_course
+		if !current_user.enrolled_in?(current_course)
+			redirect_to course_path(current_course), alert: 'You do not currently have access to this lesson. Please enroll in the course!'
+		end
 	end
 
 end
