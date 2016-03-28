039c6304f095d1357e918487b41f15996c5006e4
Risse Rigdon <risse.rigdon@gmail.com>
2016-03-28 01:09:41 +0000

    Add static pages for about and about_me, also update features on splash page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
 	def index
 	end
+
+	def about
+	end
+
+	def about_risse
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  get 'about', to: 'static_pages#about'
+  get 'about_risse', to: 'static_pages#about_risse'
 
   resource :dashboard, only: :show
 
